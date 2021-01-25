21441227fc747ff349ac64b2853ccbf1a8c3bf0c
Rizal Abdul Basith <gu4rd14n48@mail2indonesia.com>
2021-01-25 13:09:00 +0700

    nambah class soal sama nambah while di class App

--- src/App.java
+++ src/App.java
@@ -1,23 +1,34 @@
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class App {
 
     public static void main(String[] args) throws Exception {
-
-        // {"nomer e", "soal e", "jawaban sing bener", "jawaban A", "jawaban B", "jawaban C", "jawaban D"}
-        Object[][] array = { 
-            { 1, "asdasdasdasdasdasdasd", "qweqwe", "asdasd", "qweqwe", "zxczxc" },
-            { 2, "asdasdasdasdasdasdasd", "zxczxc", "asdasd", "qweqwe", "zxczxc" },
-        };
+        Soal soal = new Soal();
+        Object[][] array = soal.soal();
         // iki mek gae contoh jawaban user e, ngkok dicocokno gae fungsi ndek class Fungsi
-        Object[][] jawaban = { 
-            { 1, "qweqwe" },
-            { 2, "qweqwe" }, 
-        };
+        Object[][] jawaban = new Object[array.length][2];
+        Scanner input = new Scanner(System.in);
 
-        Object[][] hasil = Fungsi.quiz(array, jawaban);
+        // Object[][] hasil = Fungsi.quiz(array, jawaban);
 
-        System.out.println(Arrays.deepToString(hasil));
+        // System.out.println(Arrays.deepToString(hasil));
+        Boolean lanjut = true;
+        while (lanjut) {
+            System.out.println("======================================");
+            System.out.println("Soal - soalan");
+            System.out.println("");
+            System.out.println("1. Start");
+            System.out.println("2. Keluar");
+            System.out.println("======================================");
+            System.out.println("Pilih Menu :");
+            int menu = input.nextInt();
+            if (menu == 1) {
+                //iki nang quiz 1 e
+            } else {
+                lanjut = false;
+            }
+        }
 
     }
 }
---
+++ src/Soal.java
@@ -0,0 +1,10 @@
+public class Soal {
+    // {"nomer e", "soal e", "jawaban sing bener", "jawaban A", "jawaban B", "jawaban C", "jawaban D"}
+    Object[][] array = { 
+        { 1, "asdasdasdasdasdasdasd", "qweqwe", "asdasd", "qweqwe", "zxczxc" },
+        { 2, "asdasdasdasdasdasdasd", "zxczxc", "asdasd", "qweqwe", "zxczxc" },
+    };
+    public Object[][] soal() {
+        return this.array;
+    }
+}
