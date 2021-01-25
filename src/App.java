8bb0dc6603bfc375715d36808046c7e707941105
Rizal Abdul Basith <gu4rd14n48@mail2indonesia.com>
2021-01-25 11:42:28 +0700

    ganti tipe data di class App dan Fungsi

--- src/App.java
+++ src/App.java
@@ -4,10 +4,10 @@ public class App {
 
         // {"nomer e", "soal e", "jawaban sing bener", "jawaban A", "jawaban B",
         // "jawaban C", "jawaban D"}
-        String[][] array = { { "1", "asdasdasdasdasdasdasd", "qweqwe", "asdasd", "qweqwe", "zxczxc" } };
+        Object[][] array = { { 1, "asdasdasdasdasdasdasd", "qweqwe", "asdasd", "qweqwe", "zxczxc" } };
         // iki mek gae contoh jawaban user e, ngkok dicocokno gae fungsi ndek class
         // Fungsi
-        String[][] jawaban = { { "1", "asdasd" } };
+        Object[][] jawaban = { { 1, "asdasd" } };
 
     }
 }
--- src/Fungsi.java
+++ src/Fungsi.java
@@ -1,6 +1,6 @@
 public class Fungsi {
-    public static String[][] quiz(String[][] array, String[][] jawaban) {
-        String[][] result = new String[array.length][3];
+    public static Object[][] quiz(Object[][] array, Object[][] jawaban) {
+        Object[][] result = new Object[array.length][3];
         for (int a = 0; a <= array.length; a++) {
             for (int b = 0; b <= jawaban.length; b++) {
                 if (array[a][0] == jawaban[b][0]) {
