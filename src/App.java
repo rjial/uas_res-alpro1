5a0d5aa0717c50459b86913832712a74f32bd661
Rizal Abdul Basith <gu4rd14n48@mail2indonesia.com>
2021-01-25 11:10:07 +0700

    nambah komen penjelasan

--- src/App.java
+++ src/App.java
@@ -1,7 +1,12 @@
 public class App {
 
     public static void main(String[] args) throws Exception {
+
+        // {"nomer e", "soal e", "jawaban sing bener", "jawaban A", "jawaban B",
+        // "jawaban C", "jawaban D"}
         String[][] array = { { "1", "asdasdasdasdasdasdasd", "qweqwe", "asdasd", "qweqwe", "zxczxc" } };
+        // iki mek gae contoh jawaban user e, ngkok dicocokno gae fungsi ndek class
+        // Fungsi
         String[][] jawaban = { { "1", "asdasd" } };
 
     }
