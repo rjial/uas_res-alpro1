74b24045176f84f2f694df4a531dae27af80d77b
agungbayyuu <agungbayyuu@gmail.com>
2021-01-26 17:12:15 +0700

    nambah Soal Array + Jawaban

--- src/Soal.java
+++ src/Soal.java
@@ -1,8 +1,18 @@
 public class Soal {
     // {"nomer e", "soal e", "jawaban sing bener", "jawaban A", "jawaban B",
     // "jawaban C", "jawaban D"}
-    Object[][] array = { { 1, "Binatang apa yang bisa hidup di darat dan air?", "Amfibi", "Carnivora", "Mamalia",
-            "Amfibi", "Herbivora" }, { 2, "asdasdasdasdasdasdasd", "zxczxc", "asdasd", "qweqwe", "zxczxc" }, };
+    Object[][] array = { { 1, "Binatang apa yang bisa hidup di darat dan air?", "Amfibi", "Carnivora", "Mamalia","Amfibi", "Herbivora" }, 
+                        { 2, "Negara terluas keempat di dunia adalah ?","Amerika","Rusia","India","Amerika","Canada" }, 
+                        {3,"Monumen terkenal di Paris adalah ?","Menara Eiffel","Menara Eiffel","Piramida","Borobudur","Menara Pisa"},
+                        {4,"Bahan bakar kereta api adalah ?","Batubara","Avtur","Batubara","Bensin","Solar"},
+                        {5,"Gunung tertinggi di dunia adalah ?","Gunung Everest","Gunung K2","Gunung Makalu","Gunung Lhotse","Gunung Everest"},
+                        {6,"Angkor Wat terdapat di negara ?","Kamboja","Kamboja","Thailand","Vietnam","India"},
+                        {7,"Serangga yang mempunyai solidaritas tinggi ?","Semut","Lebah","Semut","Capung","Lalat"},
+                        {8,"Seni menulis arab disebut ?","Kaligrafi","Kaligrafi","Grafitti","Mural","Vandal"},
+                        {9,"Mamalia tertinggi adalah ?","Jerapah","Gajah","Kuda Nil","Jerapah","Unta"},
+                        {10,"Novel Harry Potter terbit pada tahun ?","1997","1996","1997","1998","1999"}
+
+                        };
 
     public Object[][] soal() {
         return this.array;
