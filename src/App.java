21026995c4b9257debdc826b9969606f8dc36bd6
Rizal Abdul Basith <gu4rd14n48@mail2indonesia.com>
2021-01-26 17:46:25 +0700

    nambah fungsi hasil quiz dan nilai

--- src/App.java
+++ src/App.java
@@ -27,17 +27,47 @@ public class App {
             if (menu == 1) {
                 for (int a = 0; a < array.length; a++) {
                     // Runtime.getRuntime().exec("cls");
-                    System.out.println("Quiz 1: " + array[a][1]);
-                    System.out.println("1. " + array[a][3]);
-                    System.out.println("2. " + array[a][4]);
-                    System.out.println("3. " + array[a][5]);
-                    System.out.println("4. " + array[a][6]);
-                    int pilihan = input.nextInt();
-                    Object jawab = array[a][pilihan + 2];
-                    jawaban[a][0] = array[a][0];
-                    jawaban[a][1] = jawab;
+                    Boolean lanjut_quiz = true;
+                    while (lanjut_quiz) {
+                        System.out.println("======================================");
+                        System.out.println("Quiz " + array[a][0] + ": " + array[a][1]);
+                        System.out.println("1. " + array[a][3]);
+                        System.out.println("2. " + array[a][4]);
+                        System.out.println("3. " + array[a][5]);
+                        System.out.println("4. " + array[a][6]);
+                        System.out.println("Jawaban anda [1-4] :");
+                        int pilihan = input.nextInt();
+                        if (pilihan <= 4 && pilihan >= 1) {
+                            lanjut_quiz = false;
+                            Object jawab = array[a][pilihan + 2];
+                            jawaban[a][0] = array[a][0];
+                            jawaban[a][1] = jawab;
+                        }
+                    }
                 }
                 Object[][] hasil = Fungsi.quiz(array, jawaban);
+                String salahopobener;
+                for (int a = 0; a < hasil.length; a++) {
+                    System.out.println("======================================");
+                    if ((Boolean) hasil[a][3]) {
+                        salahopobener = "Benar";
+                    } else {
+                        salahopobener = "Salah";
+                    }
+                    System.out.println("Quiz " + array[a][0] + ": " + array[a][1] + " (" + salahopobener + ")");
+                    System.out.println("Jawaban Anda : " + hasil[a][1]);
+                    System.out.println("Jawaban Yang Benar : " + hasil[a][2]);
+                }
+                System.out.println("======================================");
+                int nilai = 0;
+                for (int a = 0; a < hasil.length; a++) {
+                    if ((Boolean) hasil[a][3]) {
+                        nilai += 1;
+                    }
+                }
+                System.out.println("Nilai Anda : " + nilai);
+
+                // System.out.println(Arrays.deepToString(hasil));
             } else {
                 lanjut = false;
             }
